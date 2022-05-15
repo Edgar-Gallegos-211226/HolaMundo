ae9565f40562eb084595ab04dd48ab72c3cf4fbe
Actividad <ltchcpg@gmail.com>
2022-05-15 07:31:14 -0500

    Actualizacion de Main

---
+++ src/com/uochiapas/ejemplogithub/msin.java
@@ -0,0 +1,9 @@
+package com.uochiapas.ejemplogithub;
+
+public class msin {
+    public static void main(String[] args){
+        System.out.println("hola mundo");
+
+        System.out.println("Bienbenido al prie git que realoso");
+    }
+}
